47ba8859505f149c8df23de0675f4de8334760e8
httimes <ssgttimes735@gmail.com>
2020-05-22 11:38:15 -0500

    Guardando primera version

--- Index.php
+++ Index.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>A-Z</title>
+	<link rel="stylesheet" type="text/css" href="Vista/Apariencia/Estilos.css">
+	<title>Z-A Pagina principal</title>
 </head>
 <body>
-
+	<div class="contenedorP"> 
+		<center>
+				<h2> Bienvenidos a la A-Z</h2>
+				<a href="Vista/Ingreso.php">INGRESO</a><br>
+				<a href="Vista/Registro.php">REGISTRO</a>
+		</center>
+	</div>
 </body>
 </html>
+
+
+
+<?php echo "hola mundo"; ?>
---
+++ Vista/Ingreso.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="Apariencia/Estilos.css">
+	<title>Iniciar Sesion</title>
+</head>
+<body>
+	<div class="contenedorP">
+		<center>
+		<form action="../Controlador/ValidaIngreso.php" method="post">
+			<h3>Usuario</h3><br>
+			<input type="text" name="" placeholder="Nombre"><br>
+			<h3>Contraseña</h3>
+			<input type="password" name="" placeholder="Contraseña"><br>
+			<input type="submit" name="" value="Ingresar">	
+			<input type="reset" name="" value="Limpiar">
+		</form>
+		</center>	
+	</div>
+</body>
+</html>
